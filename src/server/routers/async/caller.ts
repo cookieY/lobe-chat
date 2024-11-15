f10aa42580bd9c13d811dcc187007a6060401116
Henry <henry@yearning.io>
2024-11-15 11:16:32 +0800

    mtls

--- src/server/routers/async/caller.ts
+++ src/server/routers/async/caller.ts
@@ -1,8 +1,11 @@
 import { createTRPCClient, httpBatchLink } from '@trpc/client';
+import fetch, { RequestInit } from 'node-fetch';
+import fs from 'node:fs';
+import { Agent } from 'node:https';
 import superjson from 'superjson';
 import urlJoin from 'url-join';
 
-// import { appEnv } from '@/config/app';
+import { appEnv } from '@/config/app';
 import { serverDBEnv } from '@/config/db';
 import { JWTPayload, LOBE_CHAT_AUTH_HEADER } from '@/const/auth';
 import { KeyVaultsGateKeeper } from '@/server/modules/KeyVaultsEncrypt';
@@ -19,13 +22,29 @@ export const createAsyncServerClient = async (userId: string, payload: JWTPayloa
   if (process.env.VERCEL_AUTOMATION_BYPASS_SECRET) {
     headers['x-vercel-protection-bypass'] = process.env.VERCEL_AUTOMATION_BYPASS_SECRET;
   }
+  let agent: Agent;
+
+  if (process.env.PFX_PATH) {
+    const pfx = fs.readFileSync(process.env.PFX_PATH);
+
+    // Configure the HTTPS agent to use the .p12 file
+    agent = new Agent({
+      passphrase: process.env.PFX_PASSPHRASE, // Use the passphrase if the .p12 file is encrypted
+      pfx, // Use the passphrase if the .p12 file is encrypted
+      rejectUnauthorized: true, // Ensure server certificate validation
+    });
+  }
 
   return createTRPCClient<AsyncRouter>({
     links: [
       httpBatchLink({
+        fetch: (url, options) => {
+          const fetchOptions = { ...options, agent } as RequestInit;
+          return fetch(url.toString(), fetchOptions);
+        },
         headers,
         transformer: superjson,
-        url: urlJoin('/trpc/async'),
+        url: urlJoin(appEnv.APP_URL!, '/trpc/async'),
       }),
     ],
   });
